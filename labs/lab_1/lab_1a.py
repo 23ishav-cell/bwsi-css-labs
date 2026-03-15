f90badcb271549f90161a10032d61167eafe6e6a
23ishav-cell <23ishav@gmail.com>
2026-03-15 12:40:43 -0400

    Add into to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Isha" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is "+name+". I'm excited for the course")
 
 if __name__ == "__main__":
     main()
