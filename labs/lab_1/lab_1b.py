2c87a80293bee070147ed5cd750e563420a919b1
23ishav-cell <23ishav@gmail.com>
2026-03-15 15:08:33 -0400

    Add user function

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -22,7 +22,6 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     Returns:
         float: The result of the operation.
     """
-
     if operation == "add":
         return num1 + num2
     elif operation == "subtract":
@@ -37,13 +36,21 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
 
+def request_sanitized_number(prompt: str) -> float:
+
+    while True:
+        try:
+            number = float(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = request_sanitized_number(input("Enter the first number: "))
+    num2 = request_sanitized_number(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
